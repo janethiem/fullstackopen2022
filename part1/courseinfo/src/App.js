1893413f196352b7a4766aedc2a6afffc99ec225
Jane Thiem <jane.thiem@circlecvi.com>
2022-04-19 17:40:22 -0600

    Exercise 1.1 refactoring out components
    Here I refactored the root to render 3 separate components.
    One for each of the header, content and total.
    Ideally, I'd like to make these components more generic,
    as Total, although having a generic
    title, is very specifically summing the
    value of exercises and printing a hard-coded message
    regarding those exercises. It may be better to just have a component
    that takes a "message" prop and three
    "value" props instead. I will continue to follow the course however and
    make this change if it makes sense to do so.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,6 @@
+import Header from './Header.js'
+import Content from './Content.js'
+import Total from './Total.js'
 
 const App = () => {
   const course = 'Half Stack application development';
@@ -10,17 +13,11 @@ const App = () => {
 
   return(
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
