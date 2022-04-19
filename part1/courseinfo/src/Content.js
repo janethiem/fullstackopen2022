2afa850fbcafb559712ecda49a44f986af6d5bb8
Jane Thiem <jane.thiem@circlecvi.com>
2022-04-19 17:54:27 -0600

    Moving all content into one component per instructions

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -1,7 +1,11 @@
 
 const Content = (props) => {
     return (
-        <p>{props.part} {props.exercises}</p>
+        <>
+            <p>{props.part1} {props.exercises1}</p>
+            <p>{props.part2} {props.exercises2}</p>
+            <p>{props.part3} {props.exercises3}</p>
+        </>
     )
 }
 
