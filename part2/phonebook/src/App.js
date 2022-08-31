9f0498f53641e75bae5722e5b1c03966362caaf5
Jane Horton Thiem <68719259+janehorton@users.noreply.github.com>
2022-08-31 16:30:55 -0600

    Exercise 2.8 phonebook step 3
    Expanded application to allow users to add phone
    numbers to the phone book by adding a second input.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,12 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([{ 
-      name: 'Arto Hellas' 
+      name: 'Arto Hellas',
+      number: '555-555-5555'
     }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -16,16 +18,17 @@ const App = () => {
     else
     {
       const personObject = {
-        name: newName 
+        name: newName,
+        number: newNumber
       };
       setPersons(persons.concat(personObject));
       setNewName('');
+      setNewNumber('');
     }
   }
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNameChange = event => setNewName(event.target.value);
+  const handleNumberChange = event => setNewNumber(event.target.value);
 
   return (
     <div>
@@ -38,14 +41,21 @@ const App = () => {
           />
         </div>
         <div>
-          <button type="submit">add</button>
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
         </div>
-        <div>debug: {newName}</div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map( person => <div key={person.name}>{person.name}</div> )}
-      </ul>
+        {
+          persons.map(person => 
+            <div key={person.name}>
+              {person.name} {person.number}
+            </div> 
+          )
+        }
     </div>
   )
 }
